a33cd3fe1cf27e42f45d57f10496a65c6613c61f
aminul <accounreate@gmail.com>
2022-12-02 02:45:10 +0600

    some route fixed

--- index.js
+++ index.js
@@ -132,11 +132,11 @@ async function run() {
          const user = await usersCollection.findOne(query);
          res.send({ isAdmin: user?.role === "Admin" });
       });
-      app.get("/users/buyer/:email", async (req, res) => {
+      app.get("/users/seller/:email", async (req, res) => {
          const email = req.params.email;
          const query = { email };
          const user = await usersCollection.findOne(query);
-         res.send({ isAdmin: user?.role === "Buyer" });
+         res.send({ isSeller: user?.role === "Seller" });
       });
 
       /* app.get("/alldelete", async (req, res) => {
