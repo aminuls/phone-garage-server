70f338f96b41e5d6bbbe540baeff839311d9355a
aminul <accounreate@gmail.com>
2022-12-02 01:04:48 +0600

    some route added

--- index.js
+++ index.js
@@ -4,22 +4,81 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 // Middle ware
-app.use(cors());
+app.use(
+   cors({
+      origin: true,
+      optionsSuccessStatus: 200,
+      credentials: true,
+   })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
    res.send("Phone Garage Server is running");
 });
 
+app.post("/create-payment-intent", async (req, res) => {
+   const booking = req.body;
+   const price = booking.price;
+   const amount = price * 100;
+
+   const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+      payment_method_types: ["card"],
+   });
+   res.send({
+      clientSecret: paymentIntent.client_secret,
+   });
+});
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+   // console.log("token", req.headers.authorization);
+   const authHeader = req.headers.authorization;
+   if (!authHeader) {
+      return res.status(401).send("Unauthorized Access");
+   }
+   const token = authHeader.split(" ")[1];
+
+   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+      if (err) {
+         return res.status(403).send({ message: "Forbidden Access" });
+      }
+      req.decoded = decoded;
+      next();
+   });
+}
+async function verifyAdmin(req, res, next) {
+   const decodedEmail = req.decoded.email;
+   const query = { email: decodedEmail };
+   const user = await usersCollection.findOne(query);
+   if (user?.role !== "Admin") {
+      return res.status(403).send({ message: "Forbidden Access" });
+   }
+   next();
+}
+
 async function run() {
    try {
       const productsCollection = client.db("phone-garage").collection("products");
+      const usersCollection = client.db("phone-garage").collection("users");
+      app.get("/jwt", async (req, res) => {
+         const email = req.query.email;
+         const query = { email: email };
+         const user = await usersCollection.findOne(query);
+         if (user) {
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: "12h" });
+            return res.send({ accessToken: token });
+         }
+         res.status(403).send({ accessToken: null });
+      });
       app.get("/category/:name", async (req, res) => {
          const categoryName = req.params.name;
          const query = { category: categoryName };
@@ -56,6 +115,28 @@ async function run() {
          const query = { advertise: true };
          const advertisedProduct = await productsCollection.find(query).project({ resale_price: 1, image: 1 }).toArray();
          res.send(advertisedProduct);
+      });
+      app.post("/users", async (req, res) => {
+         const user = req.body;
+         const query = { email: user.email };
+         const matchFind = await usersCollection.findOne(query);
+         if (matchFind) {
+            return res.send({ message: "Authentication Successful" });
+         }
+         const result = await usersCollection.insertOne(user);
+         res.send(result);
+      });
+      app.get("/users/admin/:email", async (req, res) => {
+         const email = req.params.email;
+         const query = { email };
+         const user = await usersCollection.findOne(query);
+         res.send({ isAdmin: user?.role === "Admin" });
+      });
+      app.get("/users/buyer/:email", async (req, res) => {
+         const email = req.params.email;
+         const query = { email };
+         const user = await usersCollection.findOne(query);
+         res.send({ isAdmin: user?.role === "Buyer" });
       });
 
       /* app.get("/alldelete", async (req, res) => {
