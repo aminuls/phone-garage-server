2837c0dc3c57dfeac2d61849f661ba431a4e1b76
aminul <accounreate@gmail.com>
2022-12-02 22:26:22 +0600

    some issue fixed

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post("/create-payment-intent", async (req, res) => {
    });
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -69,6 +69,7 @@ async function run() {
    try {
       const productsCollection = client.db("phone-garage").collection("products");
       const usersCollection = client.db("phone-garage").collection("users");
+      const ordersCollection = client.db("phone-garage").collection("orders");
       app.get("/jwt", async (req, res) => {
          const email = req.query.email;
          const query = { email: email };
@@ -81,18 +82,18 @@ async function run() {
       });
       app.get("/category/:name", async (req, res) => {
          const categoryName = req.params.name;
-         const query = { category: categoryName };
+         const query = { category: categoryName, available: true };
          const products = await productsCollection.find(query).toArray();
          res.send(products);
       });
       app.get("/featured", async (req, res) => {
-         const query = {};
+         const query = { available: true };
          const filter = { dateInMili: -1 };
          const products = await productsCollection.find(query).sort(filter).limit(10).toArray();
          res.send(products);
       });
       app.get("/category", async (req, res) => {
-         const query = {};
+         const query = { available: true };
          const categories = await productsCollection.find(query).project({ category: 1, image: 1 }).toArray();
          const categoryName = [];
          categories.forEach((category) => {
@@ -112,8 +113,8 @@ async function run() {
          res.send(finalResult);
       });
       app.get("/advertise", async (req, res) => {
-         const query = { advertise: true };
-         const advertisedProduct = await productsCollection.find(query).project({ resale_price: 1, image: 1 }).toArray();
+         const query = { advertise: true, available: true };
+         const advertisedProduct = await productsCollection.find(query).sort({ dateInMili: -1 }).project({ resale_price: 1, image: 1 }).toArray();
          res.send(advertisedProduct);
       });
       app.post("/users", async (req, res) => {
@@ -126,17 +127,53 @@ async function run() {
          const result = await usersCollection.insertOne(user);
          res.send(result);
       });
+      app.post("/users/orders", async (req, res) => {
+         const product = req.body;
+         const result = await ordersCollection.insertOne(product);
+         res.send(result);
+      });
       app.get("/users/admin/:email", async (req, res) => {
          const email = req.params.email;
          const query = { email };
          const user = await usersCollection.findOne(query);
          res.send({ isAdmin: user?.role === "Admin" });
       });
+      app.post("/users/seller", async (req, res) => {
+         const email = req.body.email;
+         const query = { seller_email: email };
+         const myProduct = await productsCollection.find(query).toArray();
+         // console.log(query);
+         res.send(myProduct);
+      });
+      app.delete("/users/seller/:id", async (req, res) => {
+         const id = req.params.id;
+         const filter = { _id: ObjectId(id) };
+         const deleted = await productsCollection.deleteOne(filter);
+         res.send(deleted);
+      });
+      app.put("/users/seller/ad/:id", async (req, res) => {
+         const id = req.params.id;
+         const filter = { _id: ObjectId(id) };
+         const ad = await productsCollection.findOne(filter);
+         const status = ad.advertise;
+         const updatedDoc = {
+            $set: {
+               advertise: !status,
+            },
+         };
+         const updated = await productsCollection.updateOne(filter, updatedDoc);
+         res.send(updated);
+      });
       app.get("/users/seller/:email", async (req, res) => {
          const email = req.params.email;
          const query = { email };
          const user = await usersCollection.findOne(query);
          res.send({ isSeller: user?.role === "Seller" });
+      });
+      app.post("/users/seller/addproduct", async (req, res) => {
+         const product = req.body;
+         const result = await productsCollection.insertOne(product);
+         res.send(result);
       });
 
       /* app.get("/alldelete", async (req, res) => {
